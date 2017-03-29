a3976fe47b3bccf664d43821f24372a4e83c2829
Pontus Nilsson <iceroad@neradio.com>
2017-03-29 10:49:02 +0200

    Fyllt i lite text

--- index.php
+++ index.php
@@ -35,8 +35,12 @@ wp_head();
         <section class="first-section container">        
             <div class="row">
                 <div class="col">
-                    <div class="example"></div>
-                    hejsanasdasdlaskjdlk
+                    <div class="text-center">
+                        <img style="width: 300px; height: 200px;" src="http://placehold.it/300x200">
+                        <h1>Utvecklare med koll på Wordpress baserade sajter</h1>
+                        <p class="big">Söker du en professionell webbutvecklare som kan ta hand om din Wordpress site?</p>
+                        <button class="btn">Besök mina tjänster</button>
+                    </div>
                 </div>
             </div>
         </section>
@@ -45,18 +49,27 @@ wp_head();
             <div class="container">
                 <div class="row">
                     <div class="col">
-                        <div class="example">
-                            kolumn1
+                        <div class="text-center">
+                            <img src="http://placehold.it/500x300/333333">
+                            <h3>Kodning</h3>
+                            <p>Med bra kodning kommer din hemsida fungera optimalt. Jag har 7 års erfarenhet</p>
+                            <button class="btn btn-sm">Mina tjänster</button>
                         </div>
                     </div>
                     <div class="col">
-                        <div class="example">
-                            kolumn2
+                        <div class="text-center">
+                            <img src="http://placehold.it/500x300/333333">
+                            <h3>Funktioner & Plugins</h3>
+                            <p>Saknar ni en viktig funktion på er Wordpress site? Maila mig om dina behov!</p>
+                            <button class="btn btn-sm">Kontakta mig</button>
                         </div>
                     </div>
                     <div class="col">
-                        <div class="example">
-                            kolumn3
+                        <div class="text-center">
+                            <img src="http://placehold.it/500x300/333333">
+                            <h3>Dina idér är viktiga</h3>
+                            <p>Jag lyssnar på dina behov när det kommer till ditt projekt</p>
+                            <button class="btn btn-sm">Portfolio</button>
                         </div>
                     </div>
                 </div>
